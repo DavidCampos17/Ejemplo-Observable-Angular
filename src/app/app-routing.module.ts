9ed026e64bffcf9e287a6ef8aae0c50e85112453
David <dvd.campos17@gmail.com>
2021-06-27 22:06:49 -0600

    agrego rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FormularioComponent} from './formulario/formulario.component'
-const routes: Routes = [];
+import { ListaComponent } from './lista/lista.component';
+const routes: Routes = [
+  {path:'Formulario', component:FormularioComponent},
+  {path:'Lista', component:ListaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
